d56503e849f3a04a92116ed6fdf6c583b7e50d8c
Deepak Kumar Saini <kumarsainideepak32@gmail.com>
2023-02-14 17:17:38 +0530

    First version
    First version of the practice website's home page

---
+++ templateScript.js
@@ -0,0 +1,379 @@
+let arr= [
+    {
+        name: 'UI8',
+        logoLink: 'https://cdn.dribbble.com/users/107759/avatars/mini/b718a83234554558bf21578364cc03ea.png?1676050868',
+        badge: 'TEAM',
+        numberOfLikes: 97,
+        numberOfViews: 13.1,
+        imageLink: 'https://cdn.dribbble.com/userupload/4641179/file/original-cf195fb66dca4ec35e16bb62efebbc47.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Mainframe UI',
+    },
+    {
+        name: 'Mick Champayne',
+        logoLink: 'https://cdn.dribbble.com/users/716122/avatars/mini/771451c0bb7d81445652734f78b91adf.jpeg?1669522381',
+        badge: 'PRO',
+        numberOfLikes: 80,
+        numberOfViews: 11.3,
+        imageLink: 'https://cdn.dribbble.com/userupload/4641506/file/original-1fc2d7186265d992e6c49d106eeba8f8.jpg?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Some people just...'
+    },
+    {
+        name: 'unfold',
+        logoLink: 'https://cdn.dribbble.com/users/414979/avatars/mini/1305d804ae3e46b11fd1389fac65cccf.png?1566958767',
+        badge: 'TEAM',
+        numberOfLikes: 138,
+        numberOfViews: 34.7,
+        imageLink: 'https://cdn.dribbble.com/users/930464/screenshots/20603369/0c3d7099-fc27-4045-8439-8722f9a5e7d2.gif',
+        fadedName: 'Google Hangouts',
+    },
+    {
+        name: 'Daniele Simonelli',
+        logoLink: 'https://cdn.dribbble.com/users/28275/avatars/mini/7af4beb8def59fa3d16e2f817bdbe4ea.png?1519742083',
+        badge: 'PRO',
+        numberOfLikes: 99,
+        numberOfViews: 20.9,
+        imageLink: 'https://cdn.dribbble.com/userupload/4536194/file/original-649ac0d2ffd1f33b75c3aa010433982b.jpg?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Netflix icons',    
+    },
+    {
+        name: 'Slick',
+        logoLink: 'https://cdn.dribbble.com/users/6395846/avatars/mini/9f35572ed2791986ab7b115841dc452a.png?1674567988',
+        badge: 'TEAM',
+        numberOfLikes: 158,
+        numberOfViews: 24.6,
+        imageLink: 'https://cdn.dribbble.com/userupload/4635995/file/original-6518688ccb03e8216d764a98fd930e0d.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Fintech App Design',
+    },
+    {
+        name: 'Odama',
+        logoLink: 'https://cdn.dribbble.com/users/6850340/avatars/mini/09e52d7cca4f82c18e68ba67bbaeb1dc.png?1664764970',
+        badge: 'TEAM',
+        numberOfLikes: 118,
+        numberOfViews: 2.4,
+        imageLink: 'https://cdn.dribbble.com/userupload/4644981/file/original-2cefe722976b026fb86d86fa89a831f5.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Pracima',
+    },
+    {
+        name: 'MUTI',
+        logoLink: 'https://cdn.dribbble.com/users/59947/avatars/mini/5ed2865a861fcd2bc9f9d9aff7600f8b.jpg?1574070463',
+        badge: 'PRO',
+        numberOfLikes: 132,
+        numberOfViews: 35.3,
+        imageLink: 'https://cdn.dribbble.com/userupload/4634189/file/original-6235468514ac06319386c3a26a1a226d.jpg?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Spatial Planning',
+    },
+    {
+        name: 'Vektora',
+        logoLink: 'https://cdn.dribbble.com/users/6567474/avatars/mini/b849c692c6c9fc9cfdca178b90e354d2.png?1607746416',
+        badge: 'TEAM',
+        numberOfLikes: 85,
+        numberOfViews: 1.2,
+        imageLink: 'https://cdn.dribbble.com/userupload/4642331/file/original-6940130e052c84ea37e25ce4c44ef674.png?compress=1&crop=0x0-4200x3150&resize=450x338&vertical=top',
+        fadedName: 'Ticketizy',
+    },
+    {
+        name: 'Open Purpose',
+        logoLink: 'https://cdn.dribbble.com/users/331730/avatars/mini/c942285e3ad4bdea79a0db447ff821d7.jpg?1667830066',
+        badge: 'TEAM',
+        numberOfLikes: 76,
+        numberOfViews: 9.5,
+        imageLink: 'https://cdn.dribbble.com/userupload/4573680/file/original-6642a5d54ee1755d02d644afde62da23.png?compress=1&resize=450x338&vertical=top',
+        fadedName: '[p] 1 -> N',
+    },
+    {
+        name: 'Jeroen van Eerden',
+        logoLink: 'https://cdn.dribbble.com/users/78433/avatars/mini/305e1c98dca8c28213f093b08aed255b.png?1544179193',
+        badge: 'PRO',
+        numberOfLikes: 103,
+        numberOfViews: 16.3,
+        imageLink: 'https://cdn.dribbble.com/userupload/4635891/file/original-966ba7c112bd28fe63b12b5a8474d540.jpg?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Logos from the production',
+    },
+    {
+        name: 'Kevin Kroneberger',
+        logoLink: 'https://cdn.dribbble.com/users/46461/avatars/mini/64c8e620627620831be5c86680538158.jpg?1441302604',
+        badge: 'PRO',
+        numberOfLikes: 69,
+        numberOfViews: 16.2,
+        imageLink: 'https://cdn.dribbble.com/userupload/4636457/file/original-a5156fa13d0b9537183524efd2ee4ee1.jpg?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Fieldcraft survival',
+    },
+    {
+        name: 'Pixelz',
+        logoLink: 'https://cdn.dribbble.com/users/2238039/avatars/mini/3901d2caa1c030d31e0341559d4ebaf9.png?1663034310',
+        badge: 'TEAM',
+        numberOfLikes: 85,
+        numberOfViews: 1.9,
+        imageLink: 'https://cdn.dribbble.com/userupload/4645331/file/original-a6a690b7154ca54f7be23940deefdf17.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Mage.design'
+    },
+    {
+        name: 'Mick Champayne',
+        logoLink: 'https://cdn.dribbble.com/users/716122/avatars/mini/771451c0bb7d81445652734f78b91adf.jpeg?1669522381',
+        badge: 'PRO',
+        numberOfLikes: 34,
+        numberOfViews: 3.7,
+        imageLink: 'https://cdn.dribbble.com/userupload/4641515/file/original-fe3494511a4b8d218351650ab51c9a66.jpg?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Lazy Sunday',
+    },
+    {
+        name: 'Nija Works',
+        logoLink: 'https://cdn.dribbble.com/users/8986647/avatars/mini/25dc0a8d9ee3de09346ee436c42d9d9a.png?1662658265',
+        badge: 'TEAM',
+        numberOfLikes: 82,
+        numberOfViews: 1.3,
+        imageLink: 'https://cdn.dribbble.com/userupload/4645195/file/original-4b06f1c85d4bca7b99b2f32649a430fb.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Mobilic - Electric car',
+    },
+    {
+        name: 'Anastasia Golovko',
+        logoLink: 'https://cdn.dribbble.com/users/4189231/avatars/mini/7078fc7fecf46fb9af392d0d4cdc7252.png?1660299702',
+        badge: 'PRO',
+        numberOfLikes: 272,
+        numberOfViews: 20,
+        imageLink: 'https://cdn.dribbble.com/userupload/4634458/file/original-c836eaec5b224039385d972f24892779.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'E-commerce website',
+    },
+    {
+        name: 'ever.',
+        logoLink: 'https://cdn.dribbble.com/users/13642180/avatars/mini/30fc72db8edba43727f5d7010d256667.jpg?1666835069',
+        badge: 'TEAM',
+        numberOfLikes: 131,
+        numberOfViews: 1.4,
+        imageLink: 'https://cdn.dribbble.com/userupload/4645214/file/original-0045a53d18d9f22576eacbc1a87b2fbe.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Dashboard Pos',
+    },
+    {
+        name: 'Honey Book',
+        logoLink: 'https://cdn.dribbble.com/users/1573872/avatars/mini/a7c2a1489c2e7dc1d4e40e911824e679.png?1622741048',
+        badge: 'TEAM',
+        numberOfLikes: 125,
+        numberOfViews: 1.2,
+        imageLink: 'https://cdn.dribbble.com/userupload/4608775/file/original-67f5720900c400e4e16d25e83c03cf6d.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Start organizing your business',
+    },
+    {
+        name: 'emote_agency',
+        logoLink: 'https://cdn.dribbble.com/users/11502486/avatars/mini/a8456497e26e27118763af6fc5b5fbcc.png?1650726363',
+        badge: 'TEAM',
+        numberOfLikes: 148,
+        numberOfViews: 16,
+        imageLink: 'https://cdn.dribbble.com/userupload/4614814/file/still-e02a9a88dbc7183edaacf3d158239d86.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'The Grows mobile',
+    },
+    {
+        name: 'Jordan Hughes',
+        logoLink: 'https://cdn.dribbble.com/users/2173663/avatars/mini/c4c633361e233ae7e0b9882264c95b1a.jpg?1579775888',
+        badge: 'PRO',
+        numberOfLikes: 73,
+        numberOfViews: 7.6,
+        imageLink: 'https://cdn.dribbble.com/userupload/4453791/file/original-296f40b823485241f7352e1a9cc9a990.jpg?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Command + K Search',
+    },
+    {
+        name: 'Norch Studio',
+        logoLink: 'https://cdn.dribbble.com/users/13671106/avatars/mini/5f8ce96769c3b7b75546a435dfda2e92.jpg?1667038989',
+        badge: 'TEAM',
+        numberOfLikes: 93,
+        numberOfViews: 1.4,
+        imageLink: 'https://cdn.dribbble.com/userupload/4645175/file/original-6797b7c29a269d2972eaa6b20fca617f.jpg?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Findjob - Landing Page',
+    },
+    {
+        name: 'unfold',
+        logoLink: 'https://cdn.dribbble.com/users/414979/avatars/mini/1305d804ae3e46b11fd1389fac65cccf.png?1566958767',
+        badge: 'TEAM',
+        numberOfLikes: 61, 
+        numberOfViews: 6.8,
+        imageLink: 'https://cdn.dribbble.com/userupload/4641670/file/original-e59a2fd9ec515f5c89624849892d9872.gif',
+        fadedName: 'Solana Gizzylatn'
+    },
+    {
+        name: 'Jordan Wilson',
+        logoLink: 'https://cdn.dribbble.com/users/1193653/avatars/mini/6a14bb6ab8e3c7c2122d0af8271a7d4e.png?1674630416',
+        badge: 'PRO',
+        numberOfLikes: 63,
+        numberOfViews: 8.1,
+        imageLink: 'https://cdn.dribbble.com/userupload/4625899/file/original-a9099e15d3980aeacb35909b03f7ba9e.jpg?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Motive logo Explore',
+    },
+    {
+        name: 'brian hurst',
+        logoLink: 'https://cdn.dribbble.com/users/24444/avatars/mini/27adf9f43d4d3d40f4cb8f1bfe659dea.jpg?1474582114',
+        badge: 'PRO',
+        numberOfLikes: 28,
+        numberOfViews: 4.9,
+        imageLink: 'https://cdn.dribbble.com/userupload/4641246/file/original-d89519369a033728e3947d78821c6306.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Tossed in the waves',
+    },
+    {
+        name: 'unfold',
+        logoLink: 'https://cdn.dribbble.com/users/414979/avatars/mini/1305d804ae3e46b11fd1389fac65cccf.png?1566958767',
+        badge: 'TEAM',
+        numberOfLikes: 99,
+        numberOfViews: 13.9,
+        imageLink: 'https://cdn.dribbble.com/userupload/4580945/file/original-a6fb5dd918a486b30ee4597bae355513.png?compress=1&crop=0x0-3536x2652&resize=450x338&vertical=top',
+        fadedName: 'Grizzy Illustration',
+    },
+    {
+        name: 'Sans Brothers',
+        logoLink: 'https://cdn.dribbble.com/users/9492995/avatars/mini/c9cce1e0f0881cf3794335d829f30121.jpg?1634269978',
+        badge: 'TEAM',
+        numberOfLikes: 82,
+        numberOfViews: 1.6,
+        imageLink: 'https://cdn.dribbble.com/userupload/4638099/file/original-26c95cb6bc2e4b55ef6fcd61250eff56.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Deview - Analytics',
+    },
+    {
+        name: 'Pelorous',
+        logoLink: 'https://cdn.dribbble.com/users/5429157/avatars/mini/122177b417872664368bebd7a87292ab.png?1591102411',
+        badge: 'TEAM',
+        numberOfLikes: 77,
+        numberOfViews: 1.4,
+        imageLink: 'https://cdn.dribbble.com/userupload/4629933/file/original-1624e2e8b082a7f20f3b1b01aac82579.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Redesign Apps',
+    },
+    {
+        name: 'Jordon Cheung',
+        logoLink: 'https://cdn.dribbble.com/users/238864/avatars/mini/2d8b8d141c4e14edd51e3af19a342975.jpg?1661900019',
+        badge: 'PRO',
+        numberOfLikes: 44,
+        numberOfViews: 4.8,
+        imageLink: 'https://cdn.dribbble.com/userupload/4640034/file/original-ea947de65d4424efcc5e2911ed61bf5f.png?compress=1&resize=450x338&vertical=top'
+    },
+    {
+        name: 'Halo Lab',
+        logoLink: 'https://cdn.dribbble.com/users/466659/avatars/mini/ce34610b1ea88c449a0b22a9145b082c.png?1656416422',
+        badge: 'TEAM',
+        numberOfLikes: 305,
+        numberOfViews: 26.2,
+        imageLink: 'https://cdn.dribbble.com/userupload/4630812/file/original-e4088b1b2048d8229ddd1425fbc20e77.png?compress=1&resize=450x338&vertical=top',
+        fadedName: 'Therow Website',
+    }
+
+];
+
+
+
+
+
+
+let grid = document.querySelector('.grid');
+function template(name, logo, badge, numberOfLikes, numberOfViews, image, fadedName){
+    let cardDiv = document.createElement('div');
+    cardDiv.setAttribute('class', 'card');
+    grid.appendChild(cardDiv);
+
+    let cardImgDiv = document.createElement('div');
+    cardImgDiv.setAttribute('class', 'card-img');
+    cardDiv.append(cardImgDiv);
+
+    let img = document.createElement('img');
+    img.setAttribute('src',image);
+    img.setAttribute('alt', 'Image');
+    cardImgDiv.append(img);
+
+    let imgTitleDiv = document.createElement('div');
+    imgTitleDiv.setAttribute('class', 'img-title');
+    cardImgDiv.append(imgTitleDiv);
+
+    let brandingDiv = document.createElement('div');
+    brandingDiv.setAttribute('id', 'branding');
+    let buttonsDiv = document.createElement('div');
+    buttonsDiv.setAttribute('id', 'buttons');
+    imgTitleDiv.append(brandingDiv);
+    imgTitleDiv.append(buttonsDiv);
+
+    let brandingBold = document.createElement('strong');
+    brandingBold.textContent = fadedName;
+    brandingBold.setAttribute('style','padding-left: 1rem;')
+    brandingDiv.append(brandingBold);
+
+    let buttonElement1 = document.createElement('button');
+    let buttonElement2 = document.createElement('button');
+    buttonElement1.setAttribute('id', 'folder-button');
+    buttonElement2.setAttribute('id', 'heart-button');
+    buttonsDiv.append(buttonElement1);
+    buttonsDiv.append(buttonElement2);
+    
+    let cardBodyDiv = document.createElement('div');
+    cardBodyDiv.setAttribute('class', 'card-body');
+    cardDiv.append(cardBodyDiv);
+
+    let cardTitleDiv = document.createElement('div');
+    cardTitleDiv.setAttribute('class', 'card-title');
+    cardBodyDiv.append(cardTitleDiv);
+
+    let logoImg = document.createElement('img');
+    logoImg.setAttribute('src', logo);
+    logoImg.setAttribute('alt', 'logo');
+    cardTitleDiv.append(logoImg);
+
+    let nameB = document.createElement('b');
+    nameB.textContent = name;
+    cardTitleDiv.append(nameB);
+
+    let badgeSpan = document.createElement('span');
+    badgeSpan.setAttribute('class', 'badge');
+    badgeSpan.textContent = badge;
+    cardTitleDiv.append(badgeSpan);
+
+    let cardDetailsDiv = document.createElement('div');
+    cardDetailsDiv.setAttribute('class','card-details');
+    cardBodyDiv.append(cardDetailsDiv);
+
+    let heartSpan = document.createElement('span');
+    heartSpan.setAttribute('id', 'heart-icon');
+    cardDetailsDiv.append(heartSpan);
+
+    let likeB = document.createElement('b');
+    likeB.textContent = numberOfLikes;
+    cardDetailsDiv.append(likeB);
+
+    let eyeSpan = document.createElement('span');
+    eyeSpan.setAttribute('id', 'eye-icon');
+    cardDetailsDiv.append(eyeSpan);
+
+    let viewsB = document.createElement('b');
+    viewsB.textContent = numberOfViews+'k';
+    cardDetailsDiv.append(viewsB);
+
+}
+
+arr.forEach((element) => {template(element.name, element.logoLink, element.badge, element.numberOfLikes, element.numberOfViews, element.imageLink, element.fadedName)});
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr = [
+//     "https://cdn.dribbble.com/userupload/4605110/file/original-e4e3fc4f4631d0e9761fa03c3235ee05.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4598703/file/original-66024870f8b0507620b7999b5befdf22.png?compress=1&crop=0x59-3000x2309&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4603358/file/original-3ba5e90064fb6906b99f94b84b0e2779.jpg?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4602999/file/still-e09f52a8507b121ca69896130c0cdfaf.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4603262/file/original-b5c7cafed61369d1a40965bdc598e109.jpg?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4597754/file/still-3a68ffe67cbc76db134502989b9e769f.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4603711/file/still-ae7ff351ec1c0f47abea76a9f3e40ad8.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4602615/file/original-c1e370bae9a5ebffaa0316f701924e72.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4604856/file/original-c5bd1db7b0fe46f4226f3f0cf214f1a3.jpg?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4599810/file/original-a0a1f5b0837e9a21964e513e77f59dd1.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4612698/file/original-97a5f7409cd65873993dc547d2b2bfb3.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4603607/file/original-d143b138c6c1e5329ada1ae511d2f034.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4453781/file/original-f659f18fc1945d5d9a9326b31956009f.jpg?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4609949/file/original-81ef555cf170fec63e3801f16e66f5f5.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4615706/file/still-2ac0e57d4152faedb7b9cb6abec75bb7.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4615145/file/original-07b3266e4b44135b393f3a96602fab03.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4614153/file/original-d4ee36ae31269200e15d445a4ca1f484.jpg?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4609646/file/original-cd07f9f7addbc184fedf775053280e39.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4618011/file/original-1d053197fe3e9c5b9a2531cd61135e00.png?compress=1&resize=400x300&vertical=top",
+//     "https://cdn.dribbble.com/userupload/4617670/file/original-0b6f5478463c77cb8fb154e9398b1fac.png?compress=1&resize=400x300&vertical=top",
+
+
+// ]
